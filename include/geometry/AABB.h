dc78417c72377cbc02b1ef3f50bbb3ff000d88c9
Wilberth Gonzalez <willgxnzalez@icloud.com>
2026-02-01 08:52:35 -0800

    Implemenmt axle-aligned bounding boxes for BVH

---
+++ include/geometry/AABB.h
@@ -0,0 +1,62 @@
+#pragma once
+#include "core/Vec3.h"
+#include "core/Ray.h"
+#include <iostream>
+
+/**
+ * Axis-Aligned Bounding Box
+ * Defined by minimum and maximum corners.
+ */
+struct AABB {
+    Vec3 min;
+    Vec3 max;
+
+    AABB() = default;
+    AABB(const Vec3& minCorner, const Vec3& maxCorner) : min(minCorner), max(maxCorner) {}
+
+    inline bool hit(const Ray& ray, double tMin, double tMax) const {
+        constexpr double EPS = 1e-8;
+
+        for (int axis = 0; axis < 3; ++axis) {
+            double dir = ray.direction[axis];
+            double t0, t1;
+
+            if (std::abs(dir) > EPS) { // Treat nearly parrelel rays as parellel to avoid numerical blowup 
+                double invDir = 1.0 / dir;
+                t0 = (min[axis] - ray.origin[axis]) * invDir;
+                t1 = (max[axis] - ray.origin[axis]) * invDir;
+                if (invDir < 0) std::swap(t0, t1);
+            } else { // Ray parallel to slab
+                
+                if (ray.origin[axis] < min[axis] || ray.origin[axis] > max[axis])
+                    return false;
+                t0 = -INFINITY;
+                t1 = INFINITY;
+            }
+
+            tMin = t0 > tMin ? t0 : tMin;
+            tMax = t1 < tMax ? t1 : tMax;
+
+            if (tMax < tMin)
+                return false;
+        }
+        return true;
+    }
+
+};
+
+static AABB surroundingBox(const AABB& box0, const AABB& box1) {
+    return AABB{
+        Vec3{std::min(box0.min.x, box1.min.x),
+             std::min(box0.min.y, box1.min.y),
+             std::min(box0.min.z, box1.min.z)},
+        Vec3{std::max(box0.max.x, box1.max.x),
+             std::max(box0.max.y, box1.max.y),
+             std::max(box0.max.z, box1.max.z)}
+    };
+}
+
+inline std::ostream & operator << ( std::ostream & out, const AABB & box) {
+    out << "[" << box.min << ", " << box.max << "]";
+    return out;
+}
---
+++ tests/geometry/AABBTest.cpp
@@ -0,0 +1,58 @@
+#include <gtest/gtest.h>
+#include "geometry/AABB.h"
+#include "core/Vec3.h"
+#include "core/Ray.h"
+
+TEST(AABBTest, ConstructorAndMinMax) {
+    Vec3 min{0, 0, 0};
+    Vec3 max{1, 1, 1};
+    AABB box(min, max);
+
+    EXPECT_EQ(box.min, min);
+    EXPECT_EQ(box.max, max);
+}
+
+TEST(AABBTest, HitRayInsideBox) {
+    AABB box(Vec3{0,0,0}, Vec3{1,1,1});
+    Ray ray(Vec3{0.5, 0.5, -1}, Vec3{0, 0, 1}); // Along +Z, should hit
+
+    EXPECT_TRUE(box.hit(ray, 0.0, 10.0));
+}
+
+TEST(AABBTest, MissRay) {
+    AABB box(Vec3{0,0,0}, Vec3{1,1,1});
+    Ray ray(Vec3{2, 2, 2}, Vec3{1, 0, 0}); // Completely outside
+
+    EXPECT_FALSE(box.hit(ray, 0.0, 10.0));
+}
+
+TEST(AABBTest, ParallelRayInsideSlab) {
+    AABB box(Vec3{0,0,0}, Vec3{1,1,1});
+    Ray ray(Vec3{0.5, 0.5, 0.5}, Vec3{0, 1e-10, 0}); // Almost zero direction along Y
+
+    EXPECT_TRUE(box.hit(ray, 0.0, 10.0));
+}
+
+TEST(AABBTest, ParallelRayOutsideSlab) {
+    AABB box(Vec3{0,0,0}, Vec3{1,1,1});
+    Ray ray(Vec3{2, 0.5, 0.5}, Vec3{0, 1e-10, 0}); // Parallel, outside X slab
+
+    EXPECT_FALSE(box.hit(ray, 0.0, 10.0));
+}
+
+TEST(AABBTest, SurroundingBox) {
+    AABB box1(Vec3{0,0,0}, Vec3{1,1,1});
+    AABB box2(Vec3{0.5,0.5,0.5}, Vec3{2,2,2});
+
+    AABB surrounding = surroundingBox(box1, box2);
+
+    EXPECT_EQ(surrounding.min, Vec3(0,0,0));
+    EXPECT_EQ(surrounding.max, Vec3(2,2,2));
+}
+
+TEST(AABBTest, OperatorStream) {
+    AABB box(Vec3{1,2,3}, Vec3{4,5,6});
+    std::stringstream ss;
+    ss << box;
+    EXPECT_EQ(ss.str(), "[[1, 2, 3], [4, 5, 6]]");
+}
