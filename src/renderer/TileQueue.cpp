ffd0f2409cbafd640d6600b333532974c31d1cf6
Wilberth Gonzalez <willgxnzalez@icloud.com>
2026-02-13 13:50:26 -0800

    Implement atomic TileQueue retrieval for multithreading

---
+++ src/renderer/TileQueue.cpp
@@ -0,0 +1,22 @@
+#include "renderer/TileQueue.h"
+
+
+TileQueue::TileQueue(int imageWidth, int imageHeight, int tileSize) {
+    for (int y = 0; y < imageHeight; y += tileSize) {
+        for (int x = 0; x < imageWidth; x += tileSize) {
+            tiles.push_back({
+                x,
+                y,
+                std::min(x + tileSize, imageWidth),
+                std::min(y + tileSize, imageHeight)
+            });
+        }
+    }
+}
+
+bool TileQueue::next(Tile& tile) {
+    int idx = index.fetch_add(1, std::memory_order_relaxed);
+    if (idx >= tiles.size()) return false;
+    tile = tiles[idx];
+    return true;
+}
