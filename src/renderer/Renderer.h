253f478dccdefc74c25f172588b7fd8700082335
Wilberth Gonzalez <willgxnzalez@icloud.com>
2026-02-13 22:09:17 -0800

    Add missing params to renderer methods

--- src/renderer/Renderer.h
+++ src/renderer/Renderer.h
@@ -5,11 +5,13 @@
 #include "renderer/TileQueue.h"
 #include "renderer/Scene.h"
 #include <cstdint>
+#include <string>
 
 class Renderer {
     int imageWidth_, imageHeight_;
     int samplesPerPixel_;
     int tileSize_;
+    int maxDepth_;
 
     Film film_;
     TileQueue queue_;
@@ -17,8 +19,8 @@ class Renderer {
     const uint64_t globalSeed_ = 1215;
 
 public: 
-    Renderer(int imageWidth, int imageHeight, int samplesPerPixel, int tileSize = 16);
+    Renderer(int imageWidth, int imageHeight, int samplesPerPixel, int tileSize = 16, int maxDepth = 5);
 
-    void render(const Camera& camera, const Scene& scene);
+    void render(const Camera& camera, const Scene& scene, const std::string& path);
     void renderWorker(int threadId, const Camera& camera, const Scene& scene);
 };
